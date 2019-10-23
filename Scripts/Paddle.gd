a8ecb5e515774efcf9753ba72858d38e45a63275
Jadan Ercoli <jtercoli@iu.edu>
2019-10-23 18:25:56 -0400

    Updates README, LICENSE, and the world.tscn

---
+++ Scripts/Paddle.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+func _physics_process(delta):
+	var mouse_x = get_viewport().get_mouse_position().x
+	position = Vector2(mouse_x, position.y)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
